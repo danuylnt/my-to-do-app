042274265e4f78b2fa2c08efa1ce3e13731a299f
Danu <m.danuyulianto@gmail.com>
2023-10-19 10:41:47 +0800

    Fixing

--- main.py
+++ main.py
@@ -13,15 +13,14 @@ st.subheader('Hello')
 st.write('To increase prod')
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key = todo)
+    checkbox_key = f"{todo}_{index}"
+    checkbox = st.checkbox(todo, key=checkbox_key)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[checkbox_key]
         st.experimental_rerun()
 
 st.text_input(label='',placeholder = 'Add a new todo...',
               on_change=add_todo, key='new_todo')
 
-print('Hello')
-st.session_state
